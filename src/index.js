de26bdfb22f68401bbc00829fc77835c73a7b316
aarongill745 <110789039+aarongill745@users.noreply.github.com>
2023-05-11 20:57:32 +1000

    yes

---
+++ src/index.js
@@ -0,0 +1,29 @@
+require('dotenv').config()
+const { Client, IntentsBitField } = require('discord.js')
+
+
+const client = new Client({
+    intents: [
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.MessageContent
+    ]
+})
+
+client.on('ready', (c) => {
+    console.log(`${c.user.username} is gonna do the thing.`)
+})
+
+client.on('messageCreate', (message) => {
+    console.log(message.content)
+    if (message.author.bot) {
+        return;
+    }
+    
+    if (message.content  === 'hello') {
+        message.reply('Sup bitch')
+    }
+})
+
+client.login(process.env.TOKEN);
